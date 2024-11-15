d89f4ac67327bcd9616773b834dfca3ae156abcd
Dorin olari <dorinolari96@gmail.com>
2024-11-15 16:24:41 +0200

    First commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,32 @@
+const Form = () => {
+    return (
+        <div className="flex justify-center items-center h-screen bg-indigo-600">
+            <div className="w-96 p-6 shadow-lg bg-white rounded-md">
+                <h1 className="text-3xl bloc text-center font-bold mt-11">Login</h1>
+                <hr className="mt-9"/>
+                <div className="mt-7">
+                    <input type="text" id="email"
+                           className="w-full text-base px-2 py-1 outline-none border-b-2 border-gray-600"
+                           placeholder="Email"/>
+                </div>
+                <div className="mt-8">
+                    <input type="password" id="password"
+                           className="w-full text-base px-2 py-1 outline-none border-b-2 border-gray-600"
+                           placeholder="Password"/>
+                </div>
+                <div className="mt-9">
+                    <button type="button"
+                            className="border-2 border-indigo-700 bg-indigo-700 text-white py-2.5 w-full rounded-xl hover:bg-indigo-600">Login
+                    </button>
+                </div>
+                <p className="font-semibold text-gray-600 text-center mt-11">Forgot <a href="https://www.rabota.md/ro/"
+                                                                                       className="text-blue-600 font-semibold">Password</a>?
+                </p>
+                <p className="font-semibold text-gray-600 text-center mt-2 mb-7">Don't have an account? <a
+                    href="https://999.md/ro/" className="text-blue-600 font-semibold">Sign up</a></p>
+            </div>
+        </div>
+    )
+}
+
+export default Form;
